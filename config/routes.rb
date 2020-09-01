285623218c438938769ec013f8561d7b87cd25aa
aratasuda <arata.suda@ipeinc.jp>
2020-09-01 20:21:27 +0900

    第10章

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'messages#index'
+
   get "messages/index", to: "messages#index"
-  
-  get "/", to:"messages#index", as: "root" 
+  post "messages/create", to: "messages#create"
+  get "messages/:id/edit", to: "messages#edit", as: "messages_edit"
+  patch "messages/:id/update", to: "messages#update", as: "messages_update"
+  delete "messages/:id/delete",to: "messages#destroy", as: "messages_delete"
 end
